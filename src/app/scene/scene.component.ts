a189907542adce32cd71db0ae26492ff9608e703
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-07-27 22:11:20 +0200

    Exercise 3 Bootstrap Font PT Serif
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     angular.json
    	modificados:     package-lock.json
    	modificados:     package.json
    	modificados:     src/app/scene/scene.component.ts
    	modificados:     src/styles.scss

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -3,7 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-scene',
     template: `
-        <p *ngFor="let phrase of phrases">{{ phrase }}</p>
+        <p *ngFor="let phrase of phrases" 
+            class="text-center border border-black rounded-pill border-2 p-2 mx-5 mt-2">
+            {{ phrase }}
+        </p>
     `,
     styleUrls: ['./scene.component.scss']
 })

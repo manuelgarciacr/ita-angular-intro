799b9d4ae605e1aa06269b5deb881a6bc785949f
Manuel García Criado (ITA) <manuel.garcia.cr@hotmail.com>
2023-07-27 11:45:10 +0200

    Exercise 1
     On branch master
     Changes to be committed:
    	modified:   .editorconfig
    	modified:   package.json
    	modified:   src/app/app.component.ts
    	modified:   src/app/app.module.ts
    	new file:   src/app/home/home.component.html
    	new file:   src/app/home/home.component.scss
    	new file:   src/app/home/home.component.spec.ts
    	new file:   src/app/home/home.component.ts
    	new file:   src/app/scene/scene.component.html
    	new file:   src/app/scene/scene.component.scss
    	new file:   src/app/scene/scene.component.spec.ts
    	new file:   src/app/scene/scene.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SceneComponent } from './scene/scene.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        SceneComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/scene/scene.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-scene',
+    template: `
+        <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+    `,
+    styleUrls: ['./scene.component.scss']
+})
+export class SceneComponent {
+
+}

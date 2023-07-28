39a5d71be1113f1f4fbd0273133276ab272e5204
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-07-28 17:48:53 +0200

    Clean code
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	borrados:        src/app/app.component.html
    	borrados:        src/app/app.component.scss
    	borrados:        src/app/app.component.spec.ts
    	modificados:     src/app/app.component.ts
    	borrados:        src/app/home/home.component.html
    	borrados:        src/app/home/home.component.scss
    	borrados:        src/app/home/home.component.spec.ts
    	borrados:        src/app/scene/scene.component.html
    	borrados:        src/app/scene/scene.component.scss
    	borrados:        src/app/scene/scene.component.spec.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
         <div class="container-fluid">
             <app-home class="row vh-100"/>
         </div>
-    `,
-    styleUrls: ['./app.component.scss']
+    `
 })
 export class AppComponent {
     title = 'ita-angular-intro';
